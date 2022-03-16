411f80f78cd3cfa6f580ec5ba4c7c899386ee29f
Andrei <70886263+selfc0ntrol@users.noreply.github.com>
2022-03-16 11:56:21 +0200

    Add files via upload

---
+++ Prototype/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace Prototype
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            IHappyMeal HappyMeal = new BigHappyMeal(0.7, 2);
+
+            IHappyMeal clonedHappyMeal = HappyMeal.Clone();
+
+            HappyMeal.GetInfo();
+
+            clonedHappyMeal.GetInfo();
+
+            HappyMeal = new SmallHappyMeal(0.3, 1);
+
+            clonedHappyMeal = HappyMeal.Clone();
+
+            HappyMeal.GetInfo();
+
+            clonedHappyMeal.GetInfo();
+
+            Console.Read();
+
+        }
+
+    }
+
+    interface IHappyMeal
+
+    {
+
+        IHappyMeal Clone();
+
+        void GetInfo();
+
+    }
+
+    class BigHappyMeal : IHappyMeal
+
+    {
+
+        double sizeOfPepsi;
+
+        int numberOfToys;
+
+        public BigHappyMeal(double sizeOfPepsi, int numberOfToys)
+
+        {
+
+            this.sizeOfPepsi = sizeOfPepsi;
+
+            this.numberOfToys = numberOfToys;
+
+        }
+
+        public IHappyMeal Clone()
+
+        {
+
+            return new BigHappyMeal(this.sizeOfPepsi, this.numberOfToys);
+
+        }
+
+        public void GetInfo()
+
+        {
+
+            Console.WriteLine("HappyMeal cu volumul de Pepsi {0} si numarul de jucarii {1}", sizeOfPepsi, numberOfToys);
+
+        }
+
+    }
+
+    class SmallHappyMeal : IHappyMeal
+
+    {
+
+        double sizeOfPepsi;
+
+        int numberOfToys;
+
+        public SmallHappyMeal(double sizeOfPepsi, int numberOfToys)
+
+        {
+
+            this.sizeOfPepsi = sizeOfPepsi;
+
+            this.numberOfToys = numberOfToys;
+
+        }
+
+        public IHappyMeal Clone()
+
+        {
+
+            return new SmallHappyMeal(this.sizeOfPepsi, this.numberOfToys);
+
+        }
+
+        public void GetInfo()
+
+        {
+
+            Console.WriteLine("HappyMeal cu volumul de Pepsi {0} si numarul de jucarii  {1}", sizeOfPepsi, numberOfToys);
+
+        }
+
+    }
+
+}
